a8cbe4542e06f8f78ae5b298f3e461dc5729a8e8
anieto1 <adnieto2019@gmail.com>
2025-03-16 18:02:18 -0400

    Setting up the Spring boot framework for the job tracking system

---
+++ job-tracking-system/src/main/java/com/anieto1/job_tracking_system/JobTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.anieto1.job_tracking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTrackingSystemApplication.class, args);
+	}
+
+}
